9be35aa6cb57bcc534e522618e99efa5a7b1c94e
YJ.Li <yuanjun.li@beyond-power.com>
2022-09-17 19:16:23 +0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by braindamage on 2022/9/17.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
